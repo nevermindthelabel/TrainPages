277dba97a87c05598ba565fa00a9ed77ec417758
nevermindthelabel <nevermindthelabel@gmail.com>
2018-11-17 20:59:26 -0700

    final math logic

--- app.js
+++ app.js
@@ -42,14 +42,36 @@ database.ref().on("child_added", function (childSnapshot) {
     var nextTrainName = childSnapshot.val().name;
     var nextTrainFrequency = parseInt(childSnapshot.val().frequency);
     var nextTrainDestination = childSnapshot.val().destination;
-    var nextTrainTime = moment(firstTime, 'HH:mm');
+    var nextTrainTime = moment(firstTime, 'HH:mm').subtract(1, "years");
     var now = moment();
     var timeDiff = moment().diff(moment(nextTrainTime), 'minutes');
     var timeRemaining = timeDiff % nextTrainFrequency;
     var minutesAway = nextTrainFrequency - timeRemaining;
     var nextArrival = moment().add(minutesAway, 'minutes').format('hh:mm a')
     
+    // // Assumptions
+    // var tFrequency = 3;
 
+    // // Time is 3:30 AM
+    // var firstTime = "03:30";
+
+    // // First Time (pushed back 1 year to make sure it comes before current time)
+    // var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+    // // Current Time
+    // var currentTime = moment();
+
+    // // Difference between the times
+    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % tFrequency;
+
+    // // Minute Until Train
+    // var tMinutesTillTrain = tFrequency - tRemainder;
+
+    // // Next Train
+    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
     var newRow = $("<tr>").append(
         $("<td>").text(nextTrainName),
